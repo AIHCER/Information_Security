0553b684fb2ca94f15c993539827e9480c53cdc5
scottiechou <scottiechou@gmail.com>
2019-10-19 22:07:56 +0800

    create working file

---
+++ hw2/EncryptDES.py
@@ -0,0 +1,8 @@
+import sys
+def f_function(right,key):
+    print("not yet")
+if len(sys.argv) == 3:
+    Plaintext = sys.argv[1]
+    Key = sys.argv[2]
+    Plaintext = Plaintext[2:]
+    initial = []
